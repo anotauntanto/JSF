913f54d0b190b0b40c942838d70beae7cad3e2fd
inftel08 <inftel08@192.168.183.43>
2016-02-20 20:00:33 +0100

    [UPDATE] Configuracion 1

---
+++ JSFCiudades/JSFCiudades-war/src/java/managedBean/InsertarCiudadBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author inftel08
+ */
+@ManagedBean
+@RequestScoped
+public class InsertarCiudadBean {
+
+    /**
+     * Creates a new instance of InsertarCiudadBean
+     */
+    public InsertarCiudadBean() {
+    }
+    
+}
